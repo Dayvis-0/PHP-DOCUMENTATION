e6a7d29d7272d016daaa915c05c71ddd980fbe43
Dayvis <amdayvis39@gmail.com>
2025-05-04 12:28:30 -0500

    04/05/2025 1

--- 0. Teoria PHP/3. Variables/3. Ambito de las variables/2. La palabra clave global.php
+++ 0. Teoria PHP/3. Variables/3. Ambito de las variables/2. La palabra clave global.php
@@ -16,17 +16,99 @@
     }
 
     // Uso de GLOBALS
+    $d = 2;
+    $e = 2;
+
+    function sum1() {
+        $GLOBALS['e'] = $GLOBALS['d'] + $GLOBALS['e']; 
+    }
+
+    // superglobal
+    function test_superglobal () {
+        if (isset($_POST['name'])) {
+            echo "Hola, " . $_POST['name'];
+        }
+    }
 ?>
 
 <h3>La palabra clave <code>global</code></h3>
 <p>Se usa para vincular una variable desde el ámbito global a un ámbito local</p>
 
 <?php
-    echo "<h4>Ejemplo #3 Uso de global</h4>";
+    echo "<h4>Ejemplo #3 Uso de <code>global</code></h4>";
     sum();
     echo "Muestra de b sumado dentro de sum: b = $b <br>";
-    echo "Muestra de c nueva: ", test(), "<br>";
+    echo "Muestra de c nueva que no existe en el ambito global: ", test(), "<br>";
+    echo "<p>Al declarar variables \$a y \$b globales dentro de la función, todas las referencias
+        a tales variables se referiran a la version global. </p>";
+
+    echo "<h4>Ejemplo #4 Uso de <code>GLOBALS</code></h4>";
+    sum1();
+    echo "Muestra de b sumado dentro de sum1: d = $e <br>";
+    echo "<p>El array \$GLOBALS es un array asosicativo con el nombre de la variable global como clave y 
+        los contenidos de dicha <br> variable como valor del elemento array. Existe en cualquier ámbito, esto 
+        ocurre porque es un superglobal</p>";
+
+    echo "<h4>Ejemplo #5 Ejemplo que demuestra las superglobales y el ámbito</h4>";
 ?>
+
+<form method="post">
+    <input type="text" name="name" placeholder="Tu nombre">
+    <button type="submit">Enviar</button>
+</form>
+
+<?php 
+    test_superglobal(); 
+?>
+
+<h4>Nota</h4>
+<p>Utilizar una clave <code>global</code> fuera de una función no es un error. Esta puede se <br>
+utilizada aun si el fichero esta incluido desde el interior de una funcion</p>
+
+<?php 
+    echo "<h4>Ejemplo #5 Ejemplo de la nota</h4>";
+    $x = 10;
+
+    function cargarArchivo() {
+        include 'd.php';
+    }
+
+    cargarArchivo();
+    echo "La \$x desde d: ", $x; // Muestra 100
+
+?>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th><h4>Diferencias</h4></th>
+            <th><h4><code>global</code></h4></th>
+            <th><h4><code>$GLOBALS</code></h4></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Se usa dentro de funciones</td>
+            <td>Si</td>
+            <td>Si</td>
+        </tr>
+        <tr>
+            <td>Accede a variables globales</td>
+            <td>Si</td>
+            <td>Si</td>
+        </tr>
+        <tr>
+            <td>>Necesita declaracion previa</td>
+            <td>Si, con <code>global $var</code></td>
+            <td>No, accedes directamente con <code>['var']</code></td>
+        </tr>
+        <tr>
+            <td>Se ve como un array</td>
+            <td>No</td>
+            <td>Si (asociativo)</td>
+        </tr>
+    </tbody>
+</table>
 
 <style>
     :root {
